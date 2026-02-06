5c1d3aae86eff37040a9957c837d957a612c1e9b
Hlebotron <stabasov@gmail.com>
2026-02-06 20:37:05 +0100

    Integrate interrupts into Uart Task

--- include/uart.cpp
+++ include/uart.cpp
@@ -1,17 +1,21 @@
 #include "uart.h"
 
 extern QueueHandle_t uart_out_queue;
+extern SemaphoreHandle_t uart_in_sem;
+static QueueSetHandle_t uart_set = xQueueCreateSet();
 
-int read_radio( struct RadioData& data )
+void uart_irq()
 {
-
+    BaseType_t higher_priority_task_woken = pdFALSE;
+    xSemaphoreGiveFromISR( uart_in_sem, &higher_priority_task_woken );
+    portYIELD_FROM_ISR( higher_priority_task_woken );
 }
 
 #ifdef GROUND
 
 void UartTask( void* params )
 {
-    // attachInterrupt( 35,  );
+    attachInterrupt( digitalPinToInterrupt(UART_RADIO_RX), uart_irq, FALLING );
     HardwareSerial radio_uart( 1 );
 
     radio_uart.begin( UART_RADIO_BAUD, SERIAL_8N1, UART_RADIO_RX, UART_RADIO_TX );
@@ -20,14 +24,14 @@ void UartTask( void* params )
     BaseType_t str_len = 0;
     for( ;; )
     {
+	//Forward all messages
 	while( radio_uart.available() > 0 )
 	    str += (char) radio_uart.read();
-	
+
 	while( radio_uart.availableForWrite() > 0 )
 	{
 	    
 	}
-
     }
 }
 
@@ -35,7 +39,30 @@ void UartTask( void* params )
 
 void UartTask( void* params )
 {
+    HardwareSerial radio_uart( 1 );
+    radio_uart.begin( UART_RADIO_BAUD, SERIAL_8N1, UART_RADIO_RX, UART_RADIO_TX );
+
+    xQueueAddToSet( uart_in_sem, uart_set );
+    xQueueAddToSet( uart_out_queue, uart_set );
     
+    attachInterrupt( digitalPinToInterrupt(UART_RADIO_RX), uart_irq, FALLING );
+
+    *(BaseType_t*) params = 3;
+    String str;
+    BaseType_t str_len = 0;
+    QueueSetMemberHandle_t member;
+    for( ;; )
+    {
+	member = xQueueSelectFromSet( uart_set, portMAX_DELAY );
+	if( member == uart_in_sem )
+	{
+	    
+	}
+	else if( member == uart_out_queue )
+	{
+		
+	}
+    }
 }
 
 #endif
